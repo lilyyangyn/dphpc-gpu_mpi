e06405bf295a6f44759482a418ad30f1ddc1a532
llipter <llipter@foxmail.com>
2021-10-25 22:10:22 +0200

    bug

--- gpu_libs/gpu_mpi/io.cuh
+++ gpu_libs/gpu_mpi/io.cuh
@@ -6,9 +6,14 @@
 // See documentation: https://www.mpi-forum.org/docs/mpi-4.0/mpi40-report.pdf
 
 namespace gpu_mpi {
-
-    struct MPI_Info;
-    struct MPI_File;
+    
+    struct MPI_Info{
+        // todo
+    };    
+    
+    struct MPI_File{
+        // todo
+    };
 
     __device__ int MPI_FILE_OPEN(MPI_Comm comm, const char *filename, int amode, MPI_Info info, MPI_File *fh);
 
