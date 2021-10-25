36880ed37cb88b0018b5d8e512cfa9d7372fa501
llipter <llipter@foxmail.com>
2021-10-25 22:23:12 +0200

    move it

--- gpu_libs/gpu_mpi/io.cuh
+++ gpu_libs/gpu_mpi/io.cuh
@@ -3,22 +3,22 @@
 
 #include "mpi.cuh"
 
+/**
+ * amode constants
+*/
+#define MPI_MODE_RDONLY             1
+#define MPI_MODE_RDWR               2
+#define MPI_MODE_WRONLY             4
+#define MPI_MODE_CREATE             8
+#define MPI_MODE_EXCL               16
+#define MPI_MODE_DELETE_ON_CLOSE    32
+#define MPI_MODE_UNIQUE_OPEN        64
+#define MPI_MODE_SEQUENTIAL         128
+#define MPI_MODE_APPEND             256
+
 // See documentation: https://www.mpi-forum.org/docs/mpi-4.0/mpi40-report.pdf
 
 namespace gpu_mpi {
-
-    /**
-     * amode constants
-     */
-    #define MPI_MODE_RDONLY             1
-    #define MPI_MODE_RDWR               2
-    #define MPI_MODE_WRONLY             4
-    #define MPI_MODE_CREATE             8
-    #define MPI_MODE_EXCL               16
-    #define MPI_MODE_DELETE_ON_CLOSE    32
-    #define MPI_MODE_UNIQUE_OPEN        64
-    #define MPI_MODE_SEQUENTIAL         128
-    #define MPI_MODE_APPEND             256
 
     struct MPI_Info{
         // todo
