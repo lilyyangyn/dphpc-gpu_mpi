82383d4d5f0e8220eeaf40350e23020a5b09d801
llipter <llipter@foxmail.com>
2021-10-25 21:42:54 +0200

    fix bug

--- gpu_libs/gpu_mpi/io.cu
+++ gpu_libs/gpu_mpi/io.cu
@@ -2,14 +2,6 @@
 
 namespace gpu_mpi {
 
-    struct MPI_Info{
-        // todo
-    };    
-    
-    struct MPI_File{
-        // todo
-    };
-
     __device__ int MPI_FILE_OPEN(MPI_Comm comm, const char *filename, int amode, MPI_Info info, MPI_File *fh){
         // todo
         return 0;
