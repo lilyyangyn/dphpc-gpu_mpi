44b09357f2cd1a62f948b10be1c5b77fb7bc2804
llipter <llipter@foxmail.com>
2021-10-25 22:08:21 +0200

    fix bug

--- gpu_libs/gpu_mpi/io.cuh
+++ gpu_libs/gpu_mpi/io.cuh
@@ -1,7 +1,7 @@
-#include "mpi_common.cuh"
-
 #ifndef IO_CUH
 #define IO_CUH
+
+#include "mpi.cuh"
 
 // See documentation: https://www.mpi-forum.org/docs/mpi-4.0/mpi40-report.pdf
 
@@ -9,7 +9,7 @@ namespace gpu_mpi {
 
     struct MPI_Info;
     struct MPI_File;
-    
+
     __device__ int MPI_FILE_OPEN(MPI_Comm comm, const char *filename, int amode, MPI_Info info, MPI_File *fh);
 
 }
