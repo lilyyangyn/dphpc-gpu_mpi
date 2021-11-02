388c674542eed89604c5bb822f6aab24e4c9295e
yueningyang <yyn605@outlook.com>
2021-11-02 16:33:36 +0100

    file open rough implementation

--- gpu_libs/gpu_mpi/io.cu
+++ gpu_libs/gpu_mpi/io.cu
@@ -2,40 +2,36 @@
 
 namespace gpu_mpi {
 
-    __device__ int MPI_File_open(MPI_Comm comm, const char *filename, int amode, MPI_Info info, MPI_File *fh){
-        // check amode
-        int cnt = 0;
+    __host__ __device__ int __open_file(const char *filename, int amode, MPI_File *fh){
+        FILE* file = NULL;
+
+        // check file existence
+        // TODO: file=fopen(filename,"r");
+        if(file == NULL){
+            if(!(amode & MPI_MODE_CREATE)) {
+                return MPI_ERR_NO_SUCH_FILE;
+            }
+            // No need to create new file as file will be created below
+        }else{
+            // TODO: fclose(file);
+            if(amode & MPI_MODE_EXCL) {
+                // File must not exist
+                return MPI_ERR_FILE_EXISTS;
+            }
+        }
+
+        const char *mode;
         if(amode & MPI_MODE_RDONLY){
-            cnt++;
+            mode = "r";
+        }else if(amode & MPI_MODE_RDWR){
+            mode = "r+";
+        }else if(amode & MPI_MODE_WRONLY){
+            // TODO: append or overwrite???
+            mode = "a";
         }
-        if(amode & MPI_MODE_RDWR){
-            cnt++;
-        }
-        if(amode & MPI_MODE_WRONLY){
-            cnt++;
-        }
-        if(cnt != 1){
-            // see documentation p495 line 7
-            return MPI_ERR_AMODE;
-        }
-
-        if(((amode & MPI_MODE_CREATE) || (amode & MPI_MODE_EXCL)) && (amode & MPI_MODE_RDONLY)){
-            // see documentation p495 line 8
-            return MPI_ERR_AMODE;
-        }
-
-        if((amode & MPI_MODE_RDWR) && (amode & MPI_MODE_SEQUENTIAL)){
-            // see documentation p495 line 9
-            return MPI_ERR_AMODE;
-        }
-
-        // todo: initialize fh->amode
-        // todo: initialize fh->comm
-        // todo: initialize fh->seek_pos
-        // todo: fh->seek_pos[] should be initialized to all zeros
-        // todo: initialize fh->file
         
-        // todo
+        // TODO: fh->file = fopen(filename, mode);
+
         return 0;
     }
 
@@ -44,6 +40,55 @@ namespace gpu_mpi {
         // return ftell(file);
         
         // todo: how to get the file size? how to talk to cpu?
+        return 0;
+    }
+
+    __device__ int MPI_File_open(MPI_Comm comm, const char *filename, int amode, MPI_Info info, MPI_File *fh){
+        // check amode
+        if (((amode & MPI_MODE_RDONLY) ? 1 : 0) + ((amode & MPI_MODE_RDWR) ? 1 : 0) +
+            ((amode & MPI_MODE_WRONLY) ? 1 : 0) != 1) {
+           // see documentation p495 line 7
+           return MPI_ERR_AMODE;
+        }
+        if(((amode & MPI_MODE_CREATE) || (amode & MPI_MODE_EXCL)) && (amode & MPI_MODE_RDONLY)){
+            // see documentation p495 line 8
+            return MPI_ERR_AMODE;
+        }
+        if((amode & MPI_MODE_RDWR) && (amode & MPI_MODE_SEQUENTIAL)){
+            // see documentation p495 line 9
+            return MPI_ERR_AMODE;
+        }
+
+        // create MPI_FILE
+        int rank;
+        MPI_Comm_rank(comm, &rank);
+        if(rank == 0){
+            fh->amode = amode;
+            fh->comm = comm;
+
+            // initialize fh->file
+            int status = __open_file(filename, amode, fh);
+            if(status != 0) {
+                // TODO: how to handel error code between different threads?
+            }
+            
+            // initialize fh->seek_pos
+            // TODO: MPI_MODE_UNIQUE_OPEN -> Only one seek_pos???
+            int size;
+            MPI_Comm_rank(comm, &size);
+            fh->seek_pos = new int[size];
+            int init_pos = 0;
+            if(amode & MPI_MODE_APPEND){
+                // In append mode: set pointer to end of file 
+                // see documentation p494 line 42
+                init_pos = __get_file_size(fh->file);
+            }
+            memset(fh->seek_pos, init_pos, sizeof(int) * size);
+        }
+        
+        // TODO: check whether synchronization is in need
+        __syncthreads();
+        
         return 0;
     }
 
