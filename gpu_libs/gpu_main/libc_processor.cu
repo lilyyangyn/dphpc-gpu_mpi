ea17b9431826331d13606b471977129c5febc99b
Ran Liao <llipter@foxmail.com>
2021-11-03 13:57:58 +0100

    try to execute instruction in cpu

--- gpu_libs/gpu_main/libc_processor.cu
+++ gpu_libs/gpu_main/libc_processor.cu
@@ -1,6 +1,22 @@
 #include "libc_processor.cuh"
 
+#include <stdio.h>
+
 void process_gpu_libc(void* mem, size_t size) {
+
+    // step 1, copy mem(int devide) to a host addr
+    void* buf = malloc(size);
+    cudaMemcpy(buf, mem, size, cudaMemcpyDeviceToHost); // bug
+
+    // step 2, execute the instruction
+
+
+
+
+
+    // FILE* file = fopen("/home/raliao/gpu_mpi/testfile", "w");
+    // fwrite("data\n", 1, 5, file);
+
 
 
 }
