fcd32d180dfef04ed9dd44950a495746059d36e1
Junchi Chen <junchi3526@gmail.com>
2021-11-08 22:11:53 +0100

    improve runmpicc.sh with bash shell variables

--- scripts/runmpicc.sh
+++ scripts/runmpicc.sh
@@ -1,4 +1,21 @@
-export GPU_MPI_PROJECT=/home/raliao/gpu_mpi
+
+export GPU_MPI_PROJECT=/home/$USER/dphpc-gpu_mpi
 export PATH=$PATH:/usr/local/cuda-11.3/bin/
-../../../miniconda3/bin/python3.7 ../../build/scripts/gpumpicc.py cpi.c 
-./a.out ---gpumpi -g 4 -b 1 -s 8192
+scriptdir=$(readlink -f $(dirname "$0"))
+if [ -z "$1" ]; then
+    echo "---------- no input source file, default toolchain_tests/pi ----------"
+    SOURCE_DIR="$GPU_MPI_PROJECT/toolchain_tests/pi"
+else
+    echo "---------------------- using sourcefile $1 ---------------------------"
+    SOURCE_DIR=$(readlink -f $(dirname "$1"))
+fi
+
+echo $SOURCE_DIR
+pushd .
+cd $scriptdir
+
+
+/home/$USER/miniconda3/bin/python3.7 $GPU_MPI_PROJECT/build/scripts/gpumpicc.py $SOURCE_DIR/cpi.c -o $SOURCE_DIR/pi
+
+$SOURCE_DIR/pi ---gpumpi -g 4 -b 1 -s 8192
+popd
