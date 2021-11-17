e0d646eba08b7a8160e0488bac8e522fa60382e7
yueningyang <yyn605@outlook.com>
2021-11-17 13:07:22 +0100

    bugfix: MPI_FILE_OPEN

--- gpu_libs/gpu_mpi/io.cu
+++ gpu_libs/gpu_mpi/io.cu
@@ -131,7 +131,7 @@ namespace gpu_mpi {
             // TODO: MPI_MODE_UNIQUE_OPEN -> Only one seek_pos???
             int size;
             MPI_Comm_size(comm, &size);
-            assert(size == 1);
+            // assert(size == 1);
             fh->seek_pos = (int*)malloc(size*sizeof(int));
             int init_pos = 0;
             if(amode & MPI_MODE_APPEND){
@@ -139,7 +139,11 @@ namespace gpu_mpi {
                 // see documentation p494 line 42
                 init_pos = __get_file_size(shared_fh.file);
             }
-            memset(&shared_fh.seek_pos, init_pos, sizeof(shared_fh.seek_pos));
+            // init_pos = 1;
+            for (int i = 0; i < size; i++){
+                fh->seek_pos[i] = init_pos;
+            }
+            // assert(fh->seek_pos[0] == 1);
         }
         
         __syncthreads();
