6fb5b84f3e04a686bf1f88b2e5c660184664ee74
Ran Liao <llipter@foxmail.com>
2021-11-22 21:55:41 +0100

    read at and write at

--- gpu_libs/gpu_mpi/io.cuh
+++ gpu_libs/gpu_mpi/io.cuh
@@ -107,6 +107,9 @@ namespace gpu_mpi {
     __device__ int MPI_File_read(MPI_File fh, void *buf, int count, MPI_Datatype datatype, MPI_Status *status);
     __device__ int MPI_File_write(MPI_File fh, const void *buf, int count, MPI_Datatype datatype, MPI_Status *status);
 
+    __device__ int MPI_File_read_at(MPI_File fh, MPI_Offset offset, void *buf, int count, MPI_Datatype datatype, MPI_Status *status);
+    __device__ int MPI_File_write_at(MPI_File fh, MPI_Offset offset, void *buf, int count, MPI_Datatype datatype, MPI_Status *status);
+
     // see documentation p521
     __device__ int MPI_File_get_position(MPI_File fh, MPI_Offset *offset);
     // see documentation p521
